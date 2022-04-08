6851be0dfded965a8899093487e9ba89c4a595a7
Raza Hussain Akbari <rhakbari.96@gmail.com>
2022-04-08 14:53:44 +0500

    Update projects.php

--- projects.php
+++ projects.php
@@ -59,7 +59,7 @@
                          </li>
                         
                         <li class="nav-item">
-                            <a class="nav-link js-scroll" href="https://drive.google.com/file/d/17wgJ6z6-xSgD2yddQb5abjWo3zU0QQ8E/view?usp=sharing" target="_blank">
+                            <a class="nav-link js-scroll" href="https://drive.google.com/file/d/1G_-xROybPObDKhzDPVrTXbxepTsHBPte/view?usp=sharing" target="_blank">
                                 CV / Resume</a>
                         </li>
                         <li class="nav-item">
